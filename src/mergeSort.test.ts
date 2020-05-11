d71a650c93430ded8838be80f19eb2ca18cc71f1
Lev Kowalski <levkowalski@gmail.com>
2020-05-11 15:17:13 +0200

    wip

--- src/mergeSort.test.ts
+++ src/mergeSort.test.ts
@@ -1,4 +1,4 @@
-import { mergeSort, merge, getInversionCount } from "./mergeSort";
+import { mergeSort } from "./mergeSort";
 
 describe("mergeSort", () => {
   it("should sort arrays", () => {
@@ -10,14 +10,6 @@ describe("mergeSort", () => {
   });
 });
 
-test("getInversionCount ", () => {
-  const input = [2, 1, 3, 1, 2];
-  const input2 = [7, 5, 3, 1];
-  const input3 = [1, 5, 3, 7];
-  expect(getInversionCount(input)).toEqual(4);
-  expect(getInversionCount(input2)).toEqual(6);
-  expect(getInversionCount(input3)).toEqual(1);
-});
 // describe("merge", () => {
 //   expect(merge([1, 3], [2])).toEqual([1, 2, 3]);
 // });
