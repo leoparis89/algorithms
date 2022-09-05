7d30a346e41229f1931bdc383de00059eb5faa6a
Lev Kowalski <levkowalski@gmail.com>
2022-09-05 15:08:26 +0200

    wip

--- src/linkedList.test.ts
+++ src/linkedList.test.ts
@@ -9,4 +9,10 @@ test("linkedList", () => {
   array.push("peanut");
   expect(array.length).toEqual(4);
   expect(array.get(2)).toEqual("train");
+
+  array.delete(2);
+  expect(array.length).toEqual(3);
+  expect(array.get(2)).toEqual("peanut");
+  expect(array.head?.value).toEqual("car");
+  expect(array.tail?.value).toEqual("peanut");
 });
--- src/linkedList.ts
+++ src/linkedList.ts
@@ -9,25 +9,27 @@ export class Node {
 export class LinkedList {
   head: Node | null = null;
   tail: Node | null = null;
+  length = 0;
 
   private isEmpty() {
     return this.head === null && this.tail === null;
   }
 
-  get length() {
-    let node = this.head;
+  //   get length() {
+  //     let node = this.head;
 
-    let i = 0;
-    while (node !== null) {
-      node = node.next;
-      i++;
-    }
+  //     let i = 0;
+  //     while (node !== null) {
+  //       node = node.next;
+  //       i++;
+  //     }
 
-    return i;
-  }
+  //     return i;
+  //   }
 
   push(val: string) {
     const node = new Node(val);
+    this.length++;
     if (this.isEmpty()) {
       this.head = node;
       this.tail = node;
@@ -44,7 +46,7 @@ export class LinkedList {
     }
   }
 
-  get(index: number) {
+  getNode(index: number) {
     let node = this.head;
 
     let i = 0;
@@ -53,8 +55,40 @@ export class LinkedList {
       i++;
     }
 
+    return node;
+  }
+
+  get(index: number) {
+    let node = this.getNode(index);
     return node && node.value;
   }
 
-  delete() {}
+  delete(i: number) {
+    const current = this.getNode(i);
+
+    if (current === null) {
+      return null;
+    }
+
+    const prev = this.getNode(i - 1);
+
+    if (prev == null) {
+      this.head = current;
+    } else {
+      prev.next = current.next;
+
+      if (current.next === null) {
+        this.tail = prev;
+      }
+    }
+    this.length--;
+
+    // if (i == 0) {
+    //   this.head = this.head!.next;
+    //   this.length--;
+    //   return;
+    // }
+
+    // const prev = this.get(i - 1);
+  }
 }
