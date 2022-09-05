5ebd8415aab002c1691ebac1c1b1e83419bfbfe2
Lev Kowalski <levkowalski@gmail.com>
2022-09-06 01:12:17 +0200

    wip

--- src/heap.test.ts
+++ src/heap.test.ts
@@ -5,7 +5,8 @@ test("toMaxHeap ", () => {
   const heapified = [10, 7, 9, 6, 5, 2, 8, 3, 4, 1];
   expect(toMaxHeap(initial)).toEqual(heapified);
 });
-// test("heapSort", () => {
-//   const initial = [5, 3, 2, 10, 1, 9, 8, 6, 4, 7];
-//   expect(heapSort(initial)).toEqual("");
-// });
+
+test("heapSort", () => {
+  const initial = [5, 3, 2, 10, 1, 9, 8, 6, 4, 7];
+  expect(heapSort(initial)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+});
--- src/heap.ts
+++ src/heap.ts
@@ -1,34 +1,27 @@
-export const heapify = (arr: number[], i: number, heapSize: number) => {
-  const current = arr[i];
-  const leftIndex = i * 2 + 1;
-  const rightIndex = i * 2 + 2;
-  const left = arr[leftIndex];
-  const right = arr[rightIndex];
+const swapPlace = (index1, index2, array) => {
+  let temp = array[index1];
+  array[index1] = array[index2];
+  array[index2] = temp;
+  return array;
+};
 
-  if (left && left > current) {
-    if (right && right > left) {
-      // swap right
-      arr[i] = right;
-      arr[rightIndex] = current;
-      heapify(arr, rightIndex, 0);
-    } else {
-      // swap left
-      arr[i] = left;
-      arr[leftIndex] = current;
-      heapify(arr, leftIndex, 0);
-    }
-  } else if (right && right > current) {
-    if (left && left > right) {
-      // swap left
-      arr[i] = left;
-      arr[leftIndex] = current;
-      heapify(arr, leftIndex, 0);
-    } else {
-      // swap right
-      arr[i] = right;
-      arr[rightIndex] = current;
-      heapify(arr, rightIndex, 0);
-    }
+export const heapify = (arr: number[], i: number, heapSize: number) => {
+  const left = i * 2 + 1;
+  const right = i * 2 + 2;
+
+  let largestValueIndex = i;
+
+  if (left < heapSize && arr[left] > arr[largestValueIndex]) {
+    largestValueIndex = left;
+  }
+
+  if (right < heapSize && arr[right] > arr[largestValueIndex]) {
+    largestValueIndex = right;
+  }
+
+  if (largestValueIndex !== i) {
+    swapPlace(i, largestValueIndex, arr);
+    heapify(arr, largestValueIndex, heapSize);
   }
 };
 
@@ -41,24 +34,12 @@ export const toMaxHeap = (arr: number[]) => {
   return arr;
 };
 
-const swapPlace = (index1, index2, array) => {
-  let temp = array[index1];
-  array[index1] = array[index2];
-  array[index2] = temp;
-  return array;
-};
-
 export const heapSort = (arr: number[]) => {
   const heap = toMaxHeap(arr);
-  console.log("fis", heap);
 
   for (let i = heap.length - 1; i > 0; i--) {
     swapPlace(0, i, heap);
-    // let tmp = heap[i];
-    // heap[i] = heap[0];
-    // heap[0] = tmp;
-    // console.log("calling heapiffy on idex 0", heap);
-    // heapify(heap, 0, heapSize);
+    heapify(heap, 0, i);
   }
   return heap;
 };
