07e708c9f2ef5942f6ad6a52976228490ed1b9b7
Lev Kowalski <levkowalski@gmail.com>
2022-09-06 00:40:58 +0200

    wip

--- src/heap.test.ts
+++ src/heap.test.ts
@@ -3,5 +3,6 @@ import { heapify, toMaxHeap } from "./heap";
 heapify;
 test("heapify ", () => {
   const initial = [5, 3, 2, 10, 1, 9, 8, 6, 4, 7];
-  expect(toMaxHeap(initial)).toEqual("");
+  const heapified = [10, 7, 9, 6, 5, 2, 8, 3, 4, 1];
+  expect(toMaxHeap(initial)).toEqual(heapified);
 });
